dedda1a638f8e9ef3231c0e5fdc631d9f233553c
Riju Mukherjee <rmukh561@gmail.com>
2021-10-01 00:12:30 +0530

    ✨ Added typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p className="text-black">This text should be black to prove tailwind is working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
